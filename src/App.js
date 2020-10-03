e171e5628ae3ec62b8e891577815667c89f59707
andrejapetrovic <andreja2092@gmail.com>
2020-10-03 19:57:51 +0200

    learning react hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { useForm } from "./useForm";
 import './App.css';
 
-function App() {
+const App = () => {
+	// const [{ count, count2 }, setCount] = useState({ count: 10, count2: 20 }); 
+	const [count, setCount] = useState(10);
+	const [count2, setCount2] = useState(20);
+
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const [values, handleChange] = useForm({
+		email: "", password: "", firstName: "" });
+
+	useEffect(() => {
+		console.log("Render");
+	}, [values.password, values.firstName])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+		<div>
+			<button onClick={() =>  {
+					// setCount(currentState => ({
+					// 	...currentState,
+					// 	count: currentState.count + 1
+				// }))
+					setCount(c => c + 1);
+					setCount2(c => c + 1);
+				}}>
+				Count
+			</button>
+			<div >count 1: {count}</div>
+			<div >count 2: {count2}</div>
+			<br />
+			<br />
+			<br />
+			<br />
+			<input 
+				name="email"
+				value={values.email}
+				onChange={handleChange}
+			/>
+			<input 
+				name="firstName"
+				value={values.firstName}
+				onChange={handleChange}
+			/>
+			<input 
+				type="password"
+				name="password"
+				value={values.password}
+				onChange={handleChange}
+			/>
+			<br />
+			<br />
+			{values.email}
+			<br />
+			<br />
+			{values.password}
+			<button onClick={() => console.log("Hello world")} >Hello world</button>
+		</div>
   );
 }
 
